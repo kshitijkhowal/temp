2c4737b052a595717b380eaad628c7dff67b1f0f
kshitijkhowal <kshitij.durgesh@gmail.com>
2025-01-05 23:55:39 +0530

    fixed __dirname

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use("/user",userRoute);
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(dirPath,"../frontend","dist","index.html"));
+        res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html"));
     })
 }
 
